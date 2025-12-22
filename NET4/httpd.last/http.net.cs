427e7a4fa3c2a4d7fd6e3b6cc6eb223d1629d890
Arkady <kornienko@outlook.com>
2025-12-22 14:58:41 +1000

    Add files via upload

--- NET4/httpd.last/http.net.cs
+++ NET4/httpd.last/http.net.cs
@@ -795,8 +795,8 @@ USAGE:
     http.net /unregserver             Deleting the server startup task.
 
     If necessary, Parameter and Value pairs are specified. If the value is text and contains
-    spaces, then it must be enclosed in quotation marks. You can also specify the string of
-    parameters in the xml file in the <Arguments></Arguments> section.
+    spaces, then it must be enclosed in quotation marks. You can also specify the parameters
+    string in the xml file in the <Arguments></Arguments> section.
 
 Parameters:                                                                  Values:
      -d      Folder containing the domains.                                      "+DocumentRoot+@"
