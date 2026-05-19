0c034c638cffb1c33294a5f1eb4ffbe7d734f424
Arkady <kornienko@outlook.com>
2026-05-19 13:37:54 +1000

    Create server.cs

---
+++ NET10/https/server.cs
@@ -0,0 +1,13 @@
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!                                                     !!
+//!!   https.net сервер на C#.     Автор: A.Б.Корниенко  !!
+//!!   Серверный движок            версия от 19.05.2025  !!
+//!!                                                     !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+using System;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
